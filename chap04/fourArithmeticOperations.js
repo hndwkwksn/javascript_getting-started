eb4c118f749fcea456282e7d1d773dd05ca43686
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-15 06:36:28 +0000

    [Finish]fourArithmeticOperations.js

---
+++ chap04/fourArithmeticOperations.js
@@ -0,0 +1,12 @@
+let num3 = 6;
+let num4 = 5;
+
+let ansAddition = num3 + num4;
+let ansSubtraction = num3 - num4;
+let ansMultiplication = num3 * num4;
+let ansDivision = num3 / num4;
+
+console.log(ansAddition);
+console.log(ansSubtraction);
+console.log(ansMultiplication);
+console.log(ansDivision);
