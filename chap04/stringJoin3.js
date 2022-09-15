c38acd6d192db50caf9b523df20a3ee45a175518
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-15 23:42:41 +0000

    [Finish]stringJoin3/4.js

---
+++ chap04/stringJoin3.js
@@ -0,0 +1,8 @@
+"use strict";
+let str = "計算結果は：";
+let num1 = 123;
+let num2 = 456;
+
+let ans1 = str + num1 + num2;
+
+console.log(ans1);
---
+++ chap04/stringJoin4.js
@@ -0,0 +1,8 @@
+
+let str2 = "となりました。";
+let num3 = 123;
+let num4 = 456;
+
+let ans2 = num1 + num2 + str2;
+
+console.log(ans2);
