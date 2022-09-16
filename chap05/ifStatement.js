be3149817cc64e9306c13c50d31afc5b76ee319b
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-16 02:18:59 +0000

    [Finish]5-2-1

---
+++ chap05/ifStatement.js
@@ -0,0 +1,6 @@
+"use strict";
+let num = 30;
+if(num <= 40) {
+    console.log("数値は40以下です。");
+}
+console.log("処理完了！");
---
+++ chap05/ifStatementWithRandom.js
@@ -0,0 +1,9 @@
+"use strict";
+let num1 = Math.round(Math.random() * 100);
+console.log("現在の値：" + num1);
+
+if(num1 <= 40) {
+    console.log("数値は４０以下です。");
+}
+
+console.log("処理完了！");
