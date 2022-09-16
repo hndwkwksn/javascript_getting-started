92e559794816f1793dd2ed5ad71e6f70779b7b66
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-16 02:25:56 +0000

    [Finish]5-2

---
+++ chap05/ifElseStatement.js
@@ -0,0 +1,11 @@
+"use strict";
+let num2 = Math.round(Math.random() * 100);
+console.log("現在の値は：" + num2);
+
+if(num2 <= 40) {
+    console.log("数値は40以下です。");
+} else {
+    console.log("数値は40より大きいです。");
+}
+
+console.log("処理完了！");
