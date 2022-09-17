b8bde3964a122043b8750ab88bee628b7f3898bd
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-17 06:11:03 +0000

    [Finish]6-2

---
+++ chap06/whileStatement.js
@@ -0,0 +1,9 @@
+let num = 4;
+let rand = Math.round(Math.random() * 10);
+console.log("ループ開始");
+
+while(rand !== num) {
+    console.log("randの値：" + rand);
+    rand = Math.round(Math.random() * 10);
+}
+console.log("ループ処理終了");
