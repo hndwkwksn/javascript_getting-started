319297c68517a0dfde85591a674ac3afea39ff01
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-17 06:52:35 +0000

    [Finish]6-4

---
+++ chap06/WhileAndDoWhile.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("whileループ開始");
+let j = 1;
+
+while(j < 0) {
+    console.log("jの値：" + j);
+}
+
+console.log("whileループの終了");
+
+console.log("do-whileループ開始");
+let k = 1;
+
+do {
+    console.log("kの値：" + k);
+} while(k < 0);
+
+console.log("do-whileループの終了");
