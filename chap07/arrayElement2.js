2ceea11803e20e7931d1eaf409f46e32e731acf2
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-18 10:25:31 +0000

    [Finish]7-1

---
+++ chap07/arrayElement2.js
@@ -0,0 +1,11 @@
+"use strict";
+let nameList1 = ["松田","田中","中山","山本","本田"];
+
+nameList1[5] = "田村";
+console.log("5人目は" + nameList1[4]);
+console.log("6人目は" + nameList1[5]);
+
+nameList1[8] = "村田";
+console.log("7人目は" + nameList1[6]);
+console.log("8人目は" + nameList1[7]);
+console.log("9人目は" + nameList1[8]);
