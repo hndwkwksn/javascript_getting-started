ec59611ea4c7c589e9f69ffd8f265af1362b0adb
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-19 10:46:09 +0000

    [Finish]7-2

---
+++ chap07/loopArray2.js
@@ -0,0 +1,6 @@
+"use strict";
+let nameList1 = ["松田","田中","中山","山本","本田"];
+
+for(let i = 0; i < nameList1.length; i++) {
+    console.log((i + 1) + "人目は" + nameList1[i]);
+}
---
+++ chap07/loopArray3.js
@@ -0,0 +1,6 @@
+"use strict";
+let nameList2 = ["松田","田中","中山","山本","本田"];
+
+for(let name of nameList2) {
+    console.log(name);
+}
