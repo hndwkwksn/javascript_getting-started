5a1f0700601942fc711de7152f72de5460f7c48c
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-09-24 03:45:52 +0000

    [Finish]9-3

---
+++ chap09/calcWithConstructor.js
@@ -0,0 +1,15 @@
+let takashi = new TestScoreWithConstructor("たかし", 92, 87, 74);
+takashi.printScore();
+
+let emi = new TestScoreWithConstructor("えみ", 79, 95, 83);
+emi.printScore();
+
+let takashiSum = takashi.calcSum();
+let emiSum = emi.calcSum();
+let ave2 = (takashiSum + emiSum) / 2;
+console.log("二人の合計の平均：" + ave2);
+
+let takashiAve = takashi.calcAve();
+let emiAve = emi.calcAve();
+let aveAve = (takashiAve + emiAve) / 2;
+console.log("二人の平均の平均：" + aveAve);
